5f077a581e763dd25a57a7bd3b0d201fea2d25cf
matthieu de hemptinne <matthieu.dehemptinne@gmail.com>
2023-11-09 09:20:34 +0100

    securing entry for everyone

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'users', path_names: {
+  sign_up: 'new_bros'
+  }, controllers: {
+    sessions: 'users/sessions', registrations: 'users/registrations'
+  }
   root to: "pages#home"
   get '/users/sign_out', to: 'sessions#destroy', as: :sign_out
+  get '/hidden', to: 'pages#hidden', as: :hidden
 end
