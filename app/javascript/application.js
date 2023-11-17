138f826990d6ca5acde39d85c8a070228812b361
matthieu de hemptinne <matthieu.dehemptinne@gmail.com>
2023-11-17 23:53:58 +0100

    adding carousel

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@popperjs/core"
 import "bootstrap"
+import '../assets/stylesheets/application';
+import $ from 'jquery';
+import Popper from 'popper.js';
+window.jQuery = $;
+window.$ = $;
+window.Popper = Popper;
