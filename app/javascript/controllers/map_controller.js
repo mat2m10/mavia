daa2b06af67e0f4a319e50565aef9ae2544f3005
matthieu de hemptinne <matthieu.dehemptinne@gmail.com>
2023-11-19 22:17:49 +0100

    forcing the refresh

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,7 +1,7 @@
 
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from 'mapbox-gl' // Don't forget this!
-
+//= require refresh
 export default class extends Controller {
   static values = {
     apiKey: String,
